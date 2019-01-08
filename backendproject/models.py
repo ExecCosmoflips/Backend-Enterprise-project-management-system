39118e5669cdd5094203679ee3ad3a74da017fb6
zhushiwen <467170016@qq.com>
2019-01-08 19:59:21 +0800

    Add database file for our backendproject. Ref #4

--- backendproject/models.py
+++ backendproject/models.py
@@ -16,3 +16,46 @@ class Project(models.Model):
     content = models.CharField(max_length=500, default='')
     begin_time = models.DateField(null=False, default=timezone.now)
     end_time = models.DateField(null=False, default=timezone.now)
+
+
+class Expend(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    category = models.CharField(max_length=30, default='')
+    title = models.CharField(max_length=30, default='')
+    number = models.CharField(max_length=15, blank=False)
+    agreement = models.CharField(max_length=500, default='')
+
+
+class ConfirmExpend(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    category = models.ForeignKey(Expend, on_delete=models.CASCADE)
+    number = models.CharField(max_length=15, blank=False)
+    agreement = models.CharField(max_length=500, default='')
+
+
+class Receivable(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    category = models.CharField(max_length=30, default='')
+    title = models.CharField(max_length=30, default='')
+    number = models.CharField(max_length=15, blank=False)
+    agreement = models.CharField(max_length=500, default='')
+
+
+class Advance(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    receivable = models.ForeignKey(Receivable, on_delete=models.CASCADE)
+    number = models.CharField(max_length=15, blank=False, default='0')
+    agreement = models.CharField(max_length=500, default='')
+
+
+class Income(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    category = models.ForeignKey(Receivable, on_delete=models.CASCADE)
+    confirm_num = models.CharField(max_length=15, blank=False, default='0')
+    tax_rate = models.CharField(max_length=10, blank=False)
+    invoice_document = models.CharField(max_length=500, default='')
