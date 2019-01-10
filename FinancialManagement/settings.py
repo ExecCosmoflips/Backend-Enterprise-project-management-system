302a0b7ad920ea46cc0e4a3cbf967e1f0fc24824
zhushiwen <467170016@qq.com>
2019-01-10 17:12:37 +0800

    Add static files for images in settings. Ref #12

--- FinancialManagement/settings.py
+++ FinancialManagement/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 # Add for Vue.js
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, ".../frontend/dist/static"),
